011cc44d4b9be8c3d59920bd4624e17d68a6a903
pro100sasho <65253560+pro100sasho@users.noreply.github.com>
2020-05-29 22:58:26 +0300

    Recovery

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -7,14 +7,26 @@ using System.Text;
 
 namespace Data.Entities
 {
+    /// <summary>
+    /// App implementation of IdentityUser
+    /// </summary>
     public class User : IdentityUser
     {
+        /// <summary>
+        /// Gets or Sets the first name of the User
+        /// </summary>
         [Display(Name = "First name")]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Gets or Sets the last name of the User
+        /// </summary>
         [Display(Name = "Last name")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Gets or Sets the money balance of the User
+        /// </summary>
         [DefaultValue(0.00)]
         [Range(0, double.MaxValue)]
         public decimal Balance { get; set; }
